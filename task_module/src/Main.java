9dd1dc8c035755590130460692212412514eef97
AnnaDon <doncilaas@gmail.com>
2020-11-03 16:17:13 +0200

    extra lines were removed

--- task_module/src/Main.java
+++ task_module/src/Main.java
@@ -2,10 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
     }
-
     public Main() {
     }
-
     @Override
     public String toString() {
         return "Main{}";
